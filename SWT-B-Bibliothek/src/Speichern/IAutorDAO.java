b63f8c4ae34c22e286c6f0ef22b98926212d1e98
Ole <Ole@TheMobileBeast.mshome.net>
2018-06-19 08:21:28 +0200

    fischkopp 2

--- SWT-B-Bibliothek/src/Speichern/IAutorDAO.java
+++ SWT-B-Bibliothek/src/Speichern/IAutorDAO.java
@@ -1,5 +1,14 @@
 package Speichern;
 
+import javax.sql.RowSet;
+import Fachlogik.Autor;
+
 public interface IAutorDAO {
+	public void createAutor();
+	public boolean deleteAutor();
+	public Autor getAutor();
+	public boolean updateAutor();
+	public RowSet selectAutorRowSet();
+	
 
 }
